e1c94bdd802f03fa2f426fe4517685be7218fa88
Chris <chris@chris-agnew.com>
2021-08-03 18:12:23 -0400

    ADD:Nav items

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,35 @@ const Header = () => {
 	return (
 	<div>
 	<Nav>
-			<Logo src=""/>
-
+			<Logo src="/images/logo.svg"/>
 			
-		
-		{/* <NavMenu>
-
-		</NavMenu> */}
+		<NavMenu>
+		<a href="#Home">
+			<img src="/images/home-icon.svg" alt="" />
+			<span>HOME</span>
+		</a>
+		<a href="#Search">
+			<img src="/images/search-icon.svg" alt="" />
+			<span>SEARCH</span>
+		</a>
+		<a href="#Home">
+			<img src="/images/watchlist-icon.svg" alt="" />
+			<span>WATCH LIST</span>
+		</a>
+		<a href="#Home">
+			<img src="/images/original-icon.svg" alt="" />
+			<span>ORIGINALS</span>
+		</a>
+		<a href="#Home">
+			<img src="/images/movie-icon.svg" alt="" />
+			<span>MOVIES</span>
+		</a>
+		<a href="#Home">
+			<img src="/images/series-icon.svg" alt="" />
+			<span>SERIES</span>
+		</a>
+		</NavMenu>
+		<UserImg src="/images/profilepic.jpg"/>
 		</Nav>
 	</div>	
 		
@@ -24,8 +46,42 @@ const Nav = styled.nav`
 	height: 70px;
 	background-color: #090b13;
 	color: #fff;
-`
+	display: flex;
+	align-items: center;
+	padding: 0 2rem;
+`	
 
 const Logo = styled.img`
-	width: 50px;
+	width: 100px;
 `
+
+const NavMenu = styled.div`
+	display: flex;
+	flex: 1;
+	margin-left: 25px;
+	cursor: pointer;
+	align-items: center;
+
+	a {
+		display: flex;
+		align-items: center;
+		padding: 0 12px;
+		text-decoration: none;
+		color: #fff;
+
+		img {
+			height: 30px;
+		}
+
+		span {
+			font-size: 15px;
+			letter-spacing: 1.42px;
+		}	
+	}
+	`
+	const UserImg = styled.img`
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+		cursor: pointer;
+	`
