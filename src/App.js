da43c7256e3f43d4df373d2992b8892047047f74
Chris <chris@chris-agnew.com>
2021-08-05 15:58:27 -0400

    ADD: Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import Header from './components/Header'
 import Home from './components/Home'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Details from './components/Details'
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/details">
+          <Details />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      </Router>
+      
+     
     </div>
   );
 }
