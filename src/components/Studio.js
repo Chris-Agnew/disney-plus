7f89b9af18f5624e000d97b17eb2c77d78e95116
Chris <chris@chris-agnew.com>
2021-08-06 13:32:15 -0400

    ADD: pointer cursor to when hovered

--- src/components/Studio.js
+++ src/components/Studio.js
@@ -45,14 +45,18 @@ margin-top: 50px;
 border: 3px solid rgba(249, 249, 249, 0.1);
 border-radius: 10px;
 box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+cursor: pointer;
+
 
 	img {
 		width: 100%;
 		height: 100%;
 
+
 	}
 	&:hover {
 		transform: scale(1.1);
 		border-color: rgba(249, 249, 249 0.8);
+		
 	}
 `
